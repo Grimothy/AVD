c2cf844e41bd9667a907ed4c14b666c8cb3c453d
Grimothy <charlesjcoulter@gmail.com>
2022-09-22 09:25:17 -0400

    test

--- test/aduserget.ps1
+++ test/aduserget.ps1
@@ -1,2 +1,3 @@
 import-module activedirectory 
 get-aduser -filter * -properties whencreated,lastlogon,modified
+New-ADOrganizationalUnit -Name:"Test2" -Path:"OU=Catuslab,DC=catuslab,DC=com" -ProtectedFromAccidentalDeletion:$true
